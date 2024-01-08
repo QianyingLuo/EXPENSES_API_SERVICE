b1cb7801eb996cd8cc5c2d3aed4d31ba730ad8ee
QianyingLuo <qianyingluodev@gmail.com>
2024-01-08 19:33:02 +0100

    add my first example endpoint

--- src/main.py
+++ src/main.py
@@ -1,1 +1,6 @@
-print("Hola mundo")
+from fastapi import FastAPI
+from .routes import example
+import uvicorn
+
+app = FastAPI()
+app.include_router(example.router, prefix="/example")
---
+++ src/routes/example.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+@router.get(path="/hola")
+def show_example():
+    return "Hola mundo"
